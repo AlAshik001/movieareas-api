3c05fa97978b1a71ce6f832c19bd4f63154f24d7
Al Ashik <your_email@example.com>
2025-07-22 05:00:03 +0530

    model structure successful

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -3,16 +3,30 @@
 // CURD functionality of movies
 // MVC pattern called Module View Controllers
 
+import Movie from "../models/movie.model.js";
+
 export const MovieIndex =  (req,res)=>{
     res.send("Get all movies lists");
 }
 
-export const MovieCreate = (req,res)=>{
+export const MovieCreate = async (req,res)=>{
     // id, title , descr
-        console.log(req.body)
+       
 
-        return res.json(req.body)
-    // create the movie info 
+       
+
+        // Validate your data
+   const newMovie = new Movie({
+            title: req.body.title,
+            desc: req.body.desc
+        })
+
+    try {
+         const movie = await newMovie.save()
+         return res.status(201).json(movie)
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    } 
 }
 
 export const MovieUpdate =  (req,res)=>{
