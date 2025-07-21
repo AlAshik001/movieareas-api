e5535ac598121b98c2fa93d08d81dc254a389e1d
Al Ashik <your_email@example.com>
2025-07-21 21:52:53 +0530

    route structure

--- main.js
+++ main.js
@@ -7,9 +7,36 @@ app.get("/", (req,res) =>{
 })
 
 
+
+// CURD functionality of movies
+
+// R- for Reading
+app.get('/movie', ()=>{
+
+})
+
+// C-for creating movies
+
+app.post('movies',()=>{
+
+})
+
+// U- for updating movies
+app.put('/movies/:id', ()=>{
+
+})
+
+// D- for deleting movies
+app.delete('/movies/:id', ()=>{
+
+})
+
+
 app.listen(port, ()=>{
     console.log(`The server is running at http//localhost:${port}`);
 })
+
+
 
 // DRY principle = Don't repeat again the code
 // KISS principle = Keep it so simple
